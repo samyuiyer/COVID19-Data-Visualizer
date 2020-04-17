471c2fb5d3c8c27497dad7f58fdf5c2ab1f6845b
Avery2 <JustinAveryChan@gmail.com>
2020-04-16 21:01:13 -0400

    methods to implement for 12 milestone

--- DisplayManager.java
+++ DisplayManager.java
@@ -1,10 +1,20 @@
 package ateam_final_project;
 
 public class DisplayManager {
+  
+  private DisplayPane[] displayModes;
+  private SettingsPane globalSettings;
+  
+  public void displaySetting(SettingsPane setting) {
+    
+  }
+  
+  public void displayPane(DisplayPane display) {
+    
+  }
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    
   }
 
 }
