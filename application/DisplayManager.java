d18ad717e7d1679437c3dbf1e23375561b72b283
Avery2 <JustinAveryChan@gmail.com>
2020-04-18 23:57:49 -0400

    add sliders to time range button

--- application/DisplayManager.java
+++ application/DisplayManager.java
@@ -3,6 +3,9 @@ package application;
 import java.util.List;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,6 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -19,62 +23,97 @@ import javafx.stage.Stage;
 
 public class DisplayManager extends Application {
 
-	private DisplayMode[] displayModes;
-	private SettingsPane globalSettings;
+  private DisplayMode[] displayModes;
+  private SettingsPane globalSettings;
 
-	private List<String> args;
+  private List<String> args;
 
-	private static final int WINDOW_WIDTH = 800;
-	private static final int WINDOW_HEIGHT = 600;
-	private static final String APP_TITLE = "hey its corona";
+  private static final int WINDOW_WIDTH = 800;
+  private static final int WINDOW_HEIGHT = 600;
+  private static final String APP_TITLE = "hey its corona";
+  
+  Boolean visible = true;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+  @Override
+  public void start(Stage primaryStage) throws Exception {
 
-		args = this.getParameters().getRaw();
+    args = this.getParameters().getRaw();
 
-		VBox leftPanel = new VBox();
-		leftPanel.setStyle("-fx-background-color: grey;");
-		
-		String[] dispModes = {"Table Mode", "Map Mode", "Graph Mode"};
-		
-		ComboBox<String> dpMode = new ComboBox<String>(FXCollections.observableArrayList(dispModes));
-		dpMode.setPromptText("Select Display Mode");
-		leftPanel.getChildren().add(dpMode);
-		
-		Button time = new Button("Time Range");
-		leftPanel.getChildren().add(time);
-		
-		Button loc = new Button("Locations");
-		leftPanel.getChildren().add(loc);
-		
-		ColorPicker cp = new ColorPicker();
-		leftPanel.getChildren().add(cp);
-		
-		// Main layout is Border Pane (top,left,center,right,bottom)
-        BorderPane root = new BorderPane();
-        // add to pane
-		root.setLeft(leftPanel);
-		
-		// Set Scene
-		Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+    VBox leftPanel = new VBox();
+    leftPanel.setStyle("-fx-background-color: grey;");
 
-		// Add the stuff and set the primary stage
-		primaryStage.setTitle(APP_TITLE);
-		primaryStage.setScene(mainScene);
-		primaryStage.show();
-	}
+    String[] dispModes = {"Table Mode", "Map Mode", "Graph Mode"};
 
-	public void displaySetting(SettingsPane setting) {
+    ComboBox<String> dpMode = new ComboBox<String>(FXCollections.observableArrayList(dispModes));
+    dpMode.setPromptText("Select Display Mode");
+    leftPanel.getChildren().add(dpMode);
 
-	}
+    // Slider
+    Label sliderLabel = new Label("Choose Time Range:");
+    Slider sliderStart = new Slider(0, 100, 10);
+    Slider sliderEnd = new Slider(0, 100, 90);
+    
+    sliderStart.setShowTickLabels(true);
+    sliderStart.setShowTickMarks(true);
+    sliderStart.setBlockIncrement(10);
+    sliderEnd.setShowTickLabels(true);
+    sliderEnd.setShowTickMarks(true);
+    sliderEnd.setBlockIncrement(10);
 
-	public void displayPane(DisplayMode display) {
+    sliderLabel.managedProperty().bind(sliderEnd.visibleProperty());
+    sliderStart.managedProperty().bind(sliderStart.visibleProperty());
+    sliderEnd.managedProperty().bind(sliderEnd.visibleProperty());
 
-	}
+    Button time = new Button("Time Range");
+    time.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        if (visible) {
+          sliderLabel.setVisible(false);
+          sliderStart.setVisible(false);
+          sliderEnd.setVisible(false);
+          visible = false;
+        } else {
+          sliderLabel.setVisible(true);
+          sliderStart.setVisible(true);
+          sliderEnd.setVisible(true);
+          visible = true;
+        }
+      }
+    });
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    leftPanel.getChildren().addAll(time, sliderLabel, sliderStart, sliderEnd);
+
+    Button loc = new Button("Locations");
+    leftPanel.getChildren().add(loc);
+
+    ColorPicker cp = new ColorPicker();
+    leftPanel.getChildren().add(cp);
+
+    // Main layout is Border Pane (top,left,center,right,bottom)
+    BorderPane root = new BorderPane();
+    // add to pane
+    root.setLeft(leftPanel);
+
+    // Set Scene
+    Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    // Add the stuff and set the primary stage
+    primaryStage.setTitle(APP_TITLE);
+    primaryStage.setScene(mainScene);
+    primaryStage.show();
+  }
+
+  public void displaySetting(SettingsPane setting) {
+
+  }
+
+  public void displayPane(DisplayMode display) {
+
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 
 }
