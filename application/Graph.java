abcd45bff03ce5aa4aa7439bb9354b09cf9b17e7
Avery2 <JustinAveryChan@gmail.com>
2020-04-21 02:17:15 -0400

    added a placeholder chart

--- application/Graph.java
+++ application/Graph.java
@@ -1,18 +1,23 @@
 package application;
 
 import javafx.scene.Node;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Label;
 
 public class Graph extends DisplayMode {
 
   @Override
   void reset() {
-    
+
   }
 
   @Override
   public Node getDisplayPane() {
-    return new Label("Graph");
+    final NumberAxis xAxis = new NumberAxis();
+    final NumberAxis yAxis = new NumberAxis();
+    final LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis, yAxis);
+    return lineChart;
   }
 
   @Override
