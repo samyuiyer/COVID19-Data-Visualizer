fed0f1e113a31a6d2acd43c9aae4a62fec51fb70
Avery2 <JustinAveryChan@gmail.com>
2020-04-21 02:39:18 -0400

    this kinda broken

--- application/Graph.java
+++ application/Graph.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.util.List;
 import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 
 public class Graph extends DisplayMode {
@@ -10,6 +12,7 @@ public class Graph extends DisplayMode {
   DataManager dm;
 
   Graph() {
+    super();
     try {
       dm.loadTries("data_test.txt");
     } catch (Exception e) {
@@ -26,7 +29,21 @@ public class Graph extends DisplayMode {
     final NumberAxis xAxis = new NumberAxis();
     final NumberAxis yAxis = new NumberAxis();
     final LineChart<Number, Number> chart = new LineChart<Number, Number>(xAxis, yAxis);
+    XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
+    
     chart.setTitle("Corona Time");
+    List<DataPoint> list = dm.gt.getAll();
+    int i = 0;
+    for (DataPoint d : list) {
+      series.getData().add(new XYChart.Data<Number, Number>(i, d.getDeaths()));
+      i++;
+      if (i == 100)
+        break;
+    }
+
+    chart.getData().add(series);
+
+
     return chart;
   }
 
