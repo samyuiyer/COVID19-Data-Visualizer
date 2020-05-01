5cfcc3dd10208584c40c38a27b1c3ed0ce23b514
Avery2 <JustinAveryChan@gmail.com>
2020-04-30 23:17:10 -0400

    add headers

--- application/Graph.java
+++ application/Graph.java
@@ -1,3 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Title: ateam_final_project
+//  Author: Ankur Garg, Eric Ertl, Justin Chan, Samyu Iyer, Sudeep Reddy
+//
+//  Course: CS400
+//  Semester: Spring 2020
+//  Lecture Number: 001
+//
+//  Date: 4/29/2020
+//
+//  Description:    A project that displays statistics relating to COVID-19 in a
+//                  variety of ways.
+//
+///////////////////////////////////////////////////////////////////////////////
+
 package application;
 
 import java.util.ArrayList;
